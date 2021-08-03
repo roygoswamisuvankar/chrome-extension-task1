b0a500358213562b7a8b3f5929583c2aac08f33c
suvankar roy goswami <46588797+roygoswamisuvankar@users.noreply.github.com>
2021-08-03 20:13:56 +0530

    Chrome extension1-simple calculator

---
+++ popup.js
@@ -0,0 +1,78 @@
+$(document).ready(function(){
+    $("#one").click(function(){
+        document.forms.display.value += 1;
+    });
+
+    $("#two").click(function(){
+        document.forms.display.value += 2;
+    });
+
+    $("#three").click(function(){
+        document.forms.display.value += 3;
+    });
+
+    $("#four").click(function(){
+        document.forms.display.value += 4;
+    });
+
+    $("#five").click(function(){
+        document.forms.display.value += 5;
+    });
+
+    $("#six").click(function(){
+        document.forms.display.value += 6;
+    });
+
+    $("#seven").click(function(){
+        document.forms.display.value += 7;
+    });
+
+    $("#eight").click(function(){
+        document.forms.display.value += 8;
+    });
+
+    $("#nine").click(function(){
+        document.forms.display.value += 9;
+    });
+
+    $("#zero").click(function(){
+        document.forms.display.value += 0;
+    });
+
+    $("#add").click(function(){
+        document.forms.display.value += '+'
+    });
+
+    $("#subs").click(function(){
+        document.forms.display.value += '-';
+    });
+
+    $("#multi").click(function(){
+        document.forms.display.value += '*';
+    });
+
+    $("#divide").click(function(){
+        document.forms.display.value += '/';
+    });
+
+    /*equal*/
+    $("#equal").click(function(){
+        document.forms.display.value = eval(document.forms.display.value);
+    });
+
+    /*clear function*/
+    $("#clear").click(function(){
+        document.forms.display.value = '';
+    });
+
+    /* putting ' . ' */
+    $("#dot").click(function(){
+        document.forms.display.value += '.';
+    });
+
+    /*square*/
+    $("#squr").click(function(){
+        document.forms.display.value += "**2";
+    });
+
+});
